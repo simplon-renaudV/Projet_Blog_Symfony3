b3612c93f789017f2baad1d29c75d4ed4e8b343e
simplon-patrickm <patrick.morghadi@gmx.fr>
2016-08-24 23:43:56 +0200

    réorganisation pages

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -1,12 +1,9 @@
-/*jQuery(document).ready(function()
-{
-    num_article = $(this).attr('data-id');
-    // On cache la zone de texte
-    jQuery('.contenu').hide();
-    // toggle() lorsque le lien avec l'ID #voir est cliqué
-    jQuery(num_article).click(function()
-    {
-        jQuery('.contenu').toggle(800);
-        return false;
-    });
-});*/
+
+
+$(document).ready(function(){
+ $('.contenu').hide();
+ $("a.afficher").click(function () {
+  $(this).next("div.contenu").slideToggle("slow");
+  return false;
+ });
+});
