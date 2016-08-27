3897a51b68b61924f2fbccef88bd781949a98e1b
simplon-patrickm <patrick.morghadi@gmx.fr>
2016-08-27 13:29:42 +0200

    modifier profil

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -1,21 +1,27 @@
 
 
 $(document).ready(function(){
+
+ // Afficher / Masquer  article admin
  $('.contenu').hide();
  $('.interface-admin').hide();
-
- $("a.afficher").click(function () {
+ $(".afficher").click(function () {
   $(this).next("div.contenu").toggle("slow");
   return false;
  });
 
+// Afficher menu admin
  $(".menu_admin").click(function () {
   $(this).next("div.interface-admin").toggle("slow");
+  if ($(this).hasClass('down')) {
+   $(this).removeClass('down');
+   $(this).addClass('up');
+  }
+  else {
+   $(this).removeClass('up');
+   $(this).addClass('down');
+  }
   return false;
  });
 
- $(".close").click(function () {
-  $(".interface-admin").toggle("slow");
-  return false;
- });
 });
